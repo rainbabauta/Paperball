025972154fb128d1abf2a1085e5d7a126f516935
rainbabauta <rainbabauta@yahoo.com>
2015-02-03 17:23:43 -0800

    Initial Files

---
+++ Paper game.py
@@ -0,0 +1,69 @@
+import pygame
+import math
+
+ 
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+ 
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+background_image = pygame.image.load("zelda.jpg").convert()
+player_image = pygame.image.load("player.png").convert()
+player_image.set_colorkey(BLACK)
+pygame.display.set_caption("My Game")
+click_sound = pygame.mixer.Sound("laser5.ogg")
+ 
+# Loop until the user clicks the close button.
+done = False
+
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+
+
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done = True # Flag that we are done so we exit this loop
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            click_sound.play()
+    # --- Game logic should go here
+ 
+    # --- Drawing code should go here
+ 
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+
+    screen.blit(background_image, [0, 0])
+
+    # Get the current mouse position. This returns the position
+    # as a list of two numbers.
+    player_position = pygame.mouse.get_pos()
+    x = player_position[0]
+    y = player_position[1]
+     
+    screen.blit(player_image, [x, y])
+
+       
+
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+# If you forget this line, the program will 'hang'
+# on exit if running from IDLE.
+pygame.quit()
